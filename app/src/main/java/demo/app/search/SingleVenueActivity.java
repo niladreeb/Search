0f270325ba4243f5505d72bd6d7420aa79e2ec7a
Niladree Bhattacharjee <niladreeb@gmail.com>
2016-05-05 15:58:23 +0100

    Added android app to github repository

--- app/src/main/java/demo/app/search/SingleVenueActivity.java
+++ app/src/main/java/demo/app/search/SingleVenueActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.squareup.picasso.Picasso;
 
 public class SingleVenueActivity extends AppCompatActivity {
